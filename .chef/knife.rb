775604ff7a003cab0b0b45c5d1a9415f41ebc52c
grauPuche <talk@graupuche.info>
2017-03-03 20:47:22 -0500

    complete new server

--- .chef/knife.rb
+++
@@ -1,8 +0,0 @@
-cookbook_path    ["cookbooks", "site-cookbooks"]
-node_path        "nodes"
-role_path        "roles"
-data_bag_path    "data_bags"
-ssl_verify_mode  :verify_peer
-environment_path "environments"
-
-knife[:berkshelf_path] = "cookbooks"
--- site-cookbooks/ruby/metadata.rb
+++
@@ -1,2 +0,0 @@
-name    "ruby"
-depends "rbenv"
